8ed999887b711527abe8fb95b56e173ff8f128e8
Angel Lores <lores@pdx.edu>
2026-01-13 17:46:20 -0800

    Add fizzbuzz solution

--- hw1/01-fizzbuzz.js
+++ hw1/01-fizzbuzz.js
@@ -8,6 +8,20 @@ Write a program that writes all the numbers from 1 to 100, with some exceptions:
 Use console.log() to write the proper output to the command line.
 
 **/
+for (let i = 1; i <= 100; i += 1) {
+  if (i % 15 === 0) {
+    // i divisible by both 3 and 5
+    console.log("fizzbuzz");
+  } else if (i % 3 === 0) {
+    // i divisible by 3
+    console.log("fizz");
+  } else if (i % 5 === 0) {
+    // i divisible by 5
+    console.log("buzz");
+  } else {
+    console.log(i);
+  }
+}
 
 // 1
 // 2
